d685eb4fee2c4c69df0c174284602139748028f8
n12333204-prateek <prateek.shrishailuppin@connect.qut.edu.au>
2026-05-26 14:38:43 +1000

    Update axiosConfig

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //  baseURL: 'http://localhost:5001', // local
-   baseURL: 'http://15.135.198.102', //live URL
+  baseURL: 'http://15.135.198.102:5001', //live URL
   headers: { 'Content-Type': 'application/json' },
 });
 
